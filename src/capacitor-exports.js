b0725c365ae0de614c8b73771233c0adf1393bca
sinnapasanga5 <sinnapasanga5@gmail.com>
2026-07-11 10:06:41 +0530

    Muti data edit bug fix

--- src/capacitor-exports.js
+++ src/capacitor-exports.js
@@ -1,4 +1,6 @@
 import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
 
 window.__capFilesystem = Filesystem;
 window.__capDirectory = Directory;
+window.__capShare = Share;
